56fb3a2e9f044002143516a9211cb94c6f75d49a
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-06 23:03:31 +0530

    add pages and updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expence_master_app/pages/expences.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Expences(),
     );
   }
 }

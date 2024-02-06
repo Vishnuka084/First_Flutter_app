1905769a6612060e278c880b5d92ff735a5ade61
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-06 23:17:30 +0530

    add App Bar and design

---
+++ lib/pages/expences.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expence Master"),
+        backgroundColor: const Color.fromARGB(255, 131, 18, 184),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+    );
+  }
+}

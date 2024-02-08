6717ba77b656c5903fb16d899fbc347f8c951d0d
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-08 14:42:45 +0530

    Updated Contnet

--- lib/models/expence.dart
+++ lib/models/expence.dart
@@ -1,23 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
-// create a uniqe id using uuid
+part 'expence.g.dart';
+
+//create a uuid
 final uuid = const Uuid().v4();
 
-// enum for category
-enum Category { food, travel, leasure, work }
+//date formater
+final formatedDate = DateFormat.yMd();
 
+//enum for categories
+enum Category { lowest, low, high, highst }
+
+//category icons
+final CategoryIcons = {
+  Category.lowest: Icons.lunch_dining,
+  Category.low: Icons.travel_explore,
+  Category.high: Icons.leave_bags_at_home_rounded,
+  Category.highst: Icons.work,
+};
+
+@HiveType(typeId: 1)
 class ExpenceModel {
-  //constructor
-  ExpenceModel(
-      {required this.amount,
-      required this.date,
-      required this.title,
-      required this.category})
-      : id = uuid;
+  ExpenceModel({
+    required this.id,
+    required this.title,
+    required this.decsription,
+    required this.date,
+    required this.category,
+  });
 
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
-  final double amount;
+
+  @HiveField(2)
+  final String decsription;
+
+  @HiveField(3)
   final DateTime date;
+
+  @HiveField(4)
   final Category category;
+
+  //getFrmated date
+  String get formattdeDateValue {
+    return formatedDate.format(date);
+  }
+
+  //get the id of a expence
+  // String get expenceId {
+  //   return id;
+  // }
 }
--- lib/pages/expences.dart
+++ lib/pages/expences.dart
@@ -1,68 +1,234 @@
-import 'package:expence_master_app/models/expence.dart';
-import 'package:expence_master_app/pages/widgets/add_new_expence.dart';
-import 'package:expence_master_app/pages/widgets/expence_list.dart';
+import 'package:expence_tracker_app/server/database.dart';
+import 'package:expence_tracker_app/widgets/add_expence.dart';
+import 'package:expence_tracker_app/widgets/expences_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expence_tracker_app/models/expence.dart';
+import 'package:hive/hive.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class Expences extends StatefulWidget {
-  const Expences({super.key});
+  const Expences({Key? key}) : super(key: key);
 
   @override
-  State<Expences> createState() => _ExpencesState();
+  _ExpencesState createState() => _ExpencesState();
 }
 
 class _ExpencesState extends State<Expences> {
-  //expenceList
-  final List<ExpenceModel> _expenceList = [
-    ExpenceModel(
-        amount: 12.5,
-        date: DateTime.now(),
-        title: "Foot Ball",
-        category: Category.leasure),
-    ExpenceModel(
-        amount: 10,
-        date: DateTime.now(),
-        title: "Carrot",
-        category: Category.food),
-    ExpenceModel(
-        amount: 20,
-        date: DateTime.now(),
-        title: "Bag",
-        category: Category.travel)
-  ];
+  //hive related stuff
+  final _myBox = Hive.box("expenceDatabase");
+  Database db = Database();
 
-  //function to open  a model overly
-  void _openAddExpencesOverlay() {
+  // final List<ExpenceModel> _expenseList = [
+  //   ExpenceModel(
+  //       title: "Pepper",
+  //       amount: 10,
+  //       date: DateTime.now(),
+  //       category: Category.food),
+  //   ExpenceModel(
+  //       title: "Tomato",
+  //       amount: 12.5,
+  //       date: DateTime.now(),
+  //       category: Category.travel)
+  // ];
+
+  //show the bottoModelSheet
+
+  void _openAddExpencesverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (context) {
-        return AddNewExpences();
+        return AddNewExpencce(
+          onAddExpence: addNewExpence,
+          expence: ExpenceModel(
+            id: uuid,
+            title: "",
+            decsription: "",
+            date: DateTime.now(),
+            category: Category.lowest,
+          ),
+        );
       },
     );
   }
 
+  //add new expence or edit under the expence id
+  // void addNewExpence(ExpenceModel expence) {
+  //   setState(() {
+  //     db.expenceList.add(expence);
+  //     calculateCategoryAmounts();
+  //   });
+  //   db.updateData();
+  // }
+
+  void addNewExpence(ExpenceModel expence) {
+    setState(() {
+      if (db.expenceList.contains(expence)) {
+        // Update the existing expense
+        final index = db.expenceList.indexOf(expence);
+        db.expenceList[index] = expence;
+      } else {
+        // Add the new expense with a new ID
+        db.expenceList.add(expence);
+      }
+
+      calculateCategoryAmounts();
+    });
+
+    db.updateData();
+  }
+
+  void removeExpence(ExpenceModel expence) {
+    final deletedIndex = db.expenceList.indexOf(expence);
+    ExpenceModel deletingExpende = expence;
+    setState(() {
+      db.expenceList.remove(deletingExpende);
+      calculateCategoryAmounts();
+    });
+    db.updateData();
+
+    //this will help to remove multiplles
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(milliseconds: 2000),
+        content: const Text("Expencce Deleted"),
+        action: SnackBarAction(
+          label: "undo",
+          onPressed: () {
+            setState(() {
+              db.expenceList.insert(deletedIndex, deletingExpende);
+            });
+            calculateCategoryAmounts();
+          },
+        ),
+      ),
+    );
+  }
+
+  //edit a tile
+  void editExpence(ExpenceModel expence) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return AddNewExpencce(
+          onAddExpence: addNewExpence,
+          expence: expence,
+        );
+      },
+    );
+  }
+
+// PIE CHART
+
+  double foodVal = 0;
+  double travelVal = 0;
+  double leasureVal = 0;
+  double workVal = 0;
+
+  void calculateCategoryAmounts() {
+    double foodTotal = 0;
+    double travelTotal = 0;
+    double leasureTotal = 0;
+    double workTotal = 0;
+
+    // for (final expence in db.expenceList) {
+    //   if (expence.category == Category.lowest) {
+    //     foodTotal += expence.;
+    //   }
+    //   if (expence.category == Category.low) {
+    //     leasureTotal += expence.amount;
+    //   }
+    //   if (expence.category == Category.high) {
+    //     travelTotal += expence.amount;
+    //   }
+    //   if (expence.category == Category.highst) {
+    //     workTotal += expence.amount;
+    //   }
+    // }
+
+    setState(() {
+      foodVal = foodTotal;
+      leasureVal = leasureTotal;
+      travelVal = travelTotal;
+      workVal = workTotal;
+
+      // Update the dataMap with the calculated values
+      dataMap = {
+        "Lowest": foodVal,
+        "Low": travelVal,
+        "High": leasureVal,
+        "Highest": workVal,
+      };
+    });
+  }
+
+// Initialize dataMap with the initial values
+  Map<String, double> dataMap = {
+    "Lowest": 0,
+    "Low": 0,
+    "High": 0,
+    "Highest": 0,
+  };
+
+  @override
+  void initState() {
+    super.initState();
+
+    //hive stuff for init state
+
+    //if this is the first time create the initial data
+    if (_myBox.get("EXP_DATA") == null) {
+      db.createInitialData();
+      calculateCategoryAmounts();
+    } else {
+      db.loadData();
+      calculateCategoryAmounts();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent =
+        const Center(child: Text("No data found please add some!"));
+
+    if (db.expenceList.isNotEmpty) {
+      mainContent = ExpenccesList(
+        expenseList: db.expenceList,
+        onDeleteExpence: removeExpence,
+        onEditExpence: editExpence,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Expence Master"),
-        backgroundColor: const Color.fromARGB(255, 131, 18, 184),
+        backgroundColor: const Color.fromARGB(255, 0, 81, 255),
+        title: const Text("Task Manager"),
         elevation: 0,
         actions: [
           Container(
             color: Colors.yellow,
             child: IconButton(
-              onPressed: _openAddExpencesOverlay,
+              onPressed: _openAddExpencesverlay,
               icon: const Icon(
                 Icons.add,
                 color: Colors.black,
               ),
             ),
-          ),
+          )
         ],
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ExpenceList(expenceList: _expenceList),
+          // PieChart(
+          //   animationDuration: const Duration(milliseconds: 800),
+          //   chartLegendSpacing: 32,
+          //   centerText: "Expences",
+          //   dataMap: dataMap,
+          // ),
+          mainContent
         ],
       ),
     );

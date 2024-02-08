94e6ab0f69f6e276a5127e35974a3924dae932c5
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-08 14:13:15 +0530

    Updated Contnetn And add drop button catergory updated

--- lib/pages/widgets/add_new_expence.dart
+++ lib/pages/widgets/add_new_expence.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expence_master_app/models/expence.dart';
 
 class AddNewExpences extends StatefulWidget {
   const AddNewExpences({super.key});
@@ -8,8 +9,74 @@ class AddNewExpences extends StatefulWidget {
 }
 
 class _AddNewExpenceState extends State<AddNewExpences> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _amountController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        //title text filed
+        TextField(
+          controller: _titleController,
+          decoration: const InputDecoration(
+            hintText: "Add New expence title",
+            label: Text("Tile"),
+          ),
+          keyboardType: TextInputType.text,
+          maxLength: 50,
+        ),
+        Row(
+          children: [
+            // amount
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                decoration: const InputDecoration(
+                  helperText: "Enter the amount",
+                  label: Text("Amount"),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+
+            // datapicker
+            Expanded(
+                child: Row(
+              children: [
+                Text("2023/2/3"),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.date_range_outlined),
+                ),
+              ],
+            ))
+          ],
+        ),
+
+        Row(
+          children: [
+            DropdownButton(
+              items: Category.values
+                  .map(
+                    (category) => DropdownMenuItem(
+                      value: Category,
+                      child: Text(category.name),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) {},
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
